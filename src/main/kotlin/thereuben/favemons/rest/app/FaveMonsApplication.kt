a387a54d67679c443bcf287be7a3d30623d4f963
reuben <reuben@mail>
2023-07-23 14:53:58 +0100

    removed node_modules from git

--- src/main/kotlin/thereuben/favemons/rest/app/FaveMonsApplication.kt
+++ src/main/kotlin/thereuben/favemons/rest/app/FaveMonsApplication.kt
@@ -3,9 +3,7 @@ package thereuben.favemons.rest.app
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.ComponentScan
-import org.springframework.web.servlet.mvc.SimpleControllerHandlerAdapter
 import thereuben.favemons.rest.controller.FaveMonController
 
 @ComponentScan(basePackageClasses = [FaveMonController::class])
