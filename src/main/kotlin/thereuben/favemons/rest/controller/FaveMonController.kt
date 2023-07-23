b5fde8de231cfac946c612e10784acb3d523612c
Reuben Day <reuben@Reubens-MacBook-Air.local>
2023-07-23 01:45:54 +0100

    added ui and display data with basic ag grid

--- src/main/kotlin/thereuben/favemons/rest/controller/FaveMonController.kt
+++ src/main/kotlin/thereuben/favemons/rest/controller/FaveMonController.kt
@@ -1,14 +1,40 @@
 package thereuben.favemons.rest.controller
 
+import DataReader
+import DataService
+import ValidationService
+import data.FavPkmn
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
-@RestController("api/mons")
-class FaveMonController {
+@RestController
+@RequestMapping("/fave-mons")
+class FaveMonController() {
 
-    @GetMapping("/")
-    fun test(): String {
-        return "test"
+    val dataReader = DataReader()
+    val dataService = DataService()
+    val validator = ValidationService()
+
+    fun getDataList(): List<FavPkmn> {
+        val dataList = dataReader.readCsv()
+
+        val isValidTypes = validator.validateTypes(dataList)
+        val isValidGens = validator.validateGens(dataList)
+
+        val isValidData = isValidTypes && isValidGens
+
+        if (!isValidData) {
+            println("Data is not valid.")
+            throw Exception("Data is not valid.")
+        }
+
+        return dataList
+    }
+
+    @GetMapping("/all")
+    fun test(): List<FavPkmn> {
+        return getDataList()
     }
 
 
